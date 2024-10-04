509566e4e43176eecb7ede6be3121f1541f5ea3c
Rohit Mane <rohitmane8356@gmail.com>
2024-10-05 02:01:17 +0530

    Adding Resent Key and verify-email page with User check

--- src/get-payload-clients.ts
+++ src/get-payload-clients.ts
@@ -2,10 +2,21 @@ import dotenv from 'dotenv'
 import path  from 'path'
 import type {InitOptions} from "payload/config"
 import payload from 'payload'
+import nodemailer from 'nodemailer'
 
 dotenv.config({
   path: path.resolve(__dirname,'../.env')
 
+})
+
+const transporter= nodemailer.createTransport({
+  host:"smtp.resend.com",
+  secure:true,
+  port:465,
+  auth:{
+    user:"resend",
+    pass:process.env.RESEND_API_KEY
+  }
 })
 
 let cached = (global as any).payload
@@ -32,6 +43,11 @@ const getPayloadClient =async ({initOptions}: Args={}) => {
 
     if(!cached.promise){
       cached.promise= payload.init({
+        email:{
+          transport:transporter,
+          fromAddress:"onbording@resend.dev",
+          fromName:"DigitalHippo"
+        },
         secret:process.env.PAYLOAD_SECRET,
         local: initOptions?.express ? false :true,
         ...(initOptions || {
