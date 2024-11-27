6245404192083b4bf8e5e3b3ce02e2f35fddf0a3
Rohit Mane <rohitmane8356@gmail.com>
2024-11-27 17:56:42 +0530

    Add all collection and product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -44,7 +45,7 @@ export default function Home() {
             </div>
       </div>
 
-      {/* TOdo list product */}
+     <ProductReel href="/products" title="Brand New"/>
     </MaxWidthWrapper>
     <section className="border-t border-gray-200 bg-gray-50">
       <MaxWidthWrapper className="py-20">
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,36 @@
+import { z } from "zod";
 import { authRouter } from "./auth-router";
-import {  router } from "./trpc";
+import {  publicProcedure, router } from "./trpc";
+import { QueryValidator } from "../lib/validator/query-validator";
+
+import getPayloadClient from "../get-payload-clients";
 
 
 export const appRouter= router({
   auth: authRouter,
+  getInfiniteProducts:publicProcedure.input(z.object({
+    limit:z.number().min(1).max(100),
+    cursor:z.number().nullish(),
+    query:QueryValidator
+  })).query(async({input})=>{
+    const {query,cursor}= input
+    const {sort,limit, ...queryOpts}= query
+
+    const payload= await getPayloadClient()
+
+    const {docs}= await payload.find({
+      collection:"products",
+      where:{
+        approvedForSale:{
+          equals:"approved",
+        }
+      },
+      sort,
+      depth:1,
+      limit,
+      page,
+    })
+  })
 })
 
 export type AppRouter= typeof appRouter
